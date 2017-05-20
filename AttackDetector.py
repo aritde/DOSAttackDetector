135d6820b36a8fee25a110ff7ece2e3d24332eb4
Aritra De <aritde@jerboa.soic.indiana.edu>
2017-05-19 22:26:01 -0400

    Added comments

--- AttackDetector.py
+++ AttackDetector.py
@@ -27,51 +27,75 @@ class AttackDetector(object):
 	def fraudDetection(self,recordList):
 		#print("Record is : "+ r.getIpAddress() +"Time:"+ r.gettimeStamp())
 		
-		mapOfRecords = {}
-		suspiciousIPs = set()
+		mapOfRecords = {} # responsible for maintaining the count of seen IP's in the current 2minute window
+		suspiciousIPs = set() # responsible for mainitaining the suspicious IPs
+		#The below function implements the idea:
+		"""
+			1.If IP not in map, add a new entry into the map with the following data:
+				Key : IP
+				Value : Hit Structure [timestamp:timestampOfTheIP,count:1]
+			2.If IP in map,check whether the timestamp is within the last 30 secs
+				a.If yes, increment the count value in the Hit Structure by 1
+				b.If no, update the entry in the map with the timestamp of this IP and resetting the count to 1.
+			3.If the count exceeds the threshold, add it to the set of suspicious IPs
+				
+		"""
 		for record in recordList:
 			ipAddress = record.getIpAddress()
 			print("Currently processing :"+ ipAddress)
 			currentTime=record.gettimeStamp()
+			#Implements step 2. of the above algorithm
 			if ipAddress in mapOfRecords:
-				lastHitTime = mapOfRecords[ipAddress].gettimeStamp()
-				difference = self.calculateTimeDifference(lastHitTime,currentTime)
+				lastHitTime = mapOfRecords[ipAddress].gettimeStamp()#fetches the last timestamp this IP was seen in the 2 minute window 
+				difference = self.calculateTimeDifference(lastHitTime,currentTime)#calculates the difference between current record and last seen same IP record
 				#print("Difference :"+ str(difference))
 				diffSeconds = int(difference/1000%60)
 				diffMinutes = int(difference/(60*1000)%60)
 				diffHours = int(difference/(60*60*1000))
 				#print(str(diffSeconds) +" "+str(diffMinutes)+" "+str(diffHours))
-				if diffHours == 0 and diffMinutes<1:	
+				#Implements step 2a. of the above algortihm
+				if diffHours == 0 and diffMinutes == 0 and diffSeconds <=30:	
 					updateCount = mapOfRecords[ipAddress].getCount();
 					h=HitStructure(lastHitTime,updateCount+1)
 					mapOfRecords[ipAddress]=h
-					if updateCount+1>=89:
+					#Implements step 3 of the above algortihm
+					if updateCount+1>=85:
 						if ipAddress not in suspiciousIPs:
 							suspiciousIPs.add(ipAddress)
+				#Implements step 2b. of the above algortihm 
+				else:
+					h=HitStructure(lastHitTime,1)
+			#Implements step 1 of the above algorithm
 			else:
 				h = HitStructure(currentTime,1)
 				mapOfRecords[ipAddress]=h
+		#Displays the suspicious IP count on the console
 		print("Suspicious IPs : "+str(len(suspiciousIPs)))
+		#Redirects the output to an output file
 		self.writeOutput(suspiciousIPs)
-
+	
+	#Function to check if output file exists from the previous run. If yes, overwrites the output file with the new output
 	def writeOutput(self,suspiciousIPs):
 		outputFile=os.path.expanduser('~') + '/dosDetector/suspicious.txt'
 		try:
-    			os.remove(outputFile)
+    			os.remove(outputFile)#If the file already exists, remove it
 		except OSError:
     			pass
 		fileHandler = open(outputFile,'a')
-		#outputFile=open("suspicious.txt","w+")
+		#Write the unique list of suspicious IP's onto a different file
 		for ip in suspiciousIPs:
 			fileHandler.write(ip+"\n")
 		fileHandler.close()
 		
+	#Function to accept a line and populate the Record Data Structure for easy access
 	def splitFields(self,line):
 		fields = line.split(" ")
 		timestamp=fields[3].split(":",1)[1].split(" ", 1)[0]
 		r = Record(fields[0],timestamp);
 		return r
+#Main function
 def main():
+	#Creates an instance of the above class
 	attackDetector = AttackDetector()
 	attackDetector.loadInput()
 
