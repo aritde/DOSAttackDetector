38fbd4553c2dee99dac5909af301608400a9a637
Aritra De <aritde@jerboa.soic.indiana.edu>
2017-05-22 12:55:34 -0400

    Adding unit test for splitfields

--- AttackDetectorTests.py
+++ AttackDetectorTests.py
@@ -1,9 +1,14 @@
-from AttackDetector import AttackDetector 
+from AttackDetector import AttackDetector
+from Record import Record
 import unittest
 
 class AttackDetectorTests(unittest.TestCase):
 	def test_calculateTimeDifference(self):
 		obj = AttackDetector()
 		self.assertEqual(obj.calculateTimeDifference('23:03:15','23:07:15'),240000)
+	def test_splitFields(self):
+		obj = AttackDetector()
+		r = obj.splitFields("200.4.91.190 - - [25/May/2015:23:11:15 +0000] \"GET / HTTP/1.0\" 200 3557 \"-\" \"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)")
+		self.assertIsInstance(r,Record)
 if __name__ == '__main__':
 	unittest.main()
